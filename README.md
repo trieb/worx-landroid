1806f579966f7649df7adc6ca75f4512642d2213
Mikael Trieb <mikael@trieb.se>
2015-06-23 22:27:58 +0200

    Parse alarms and publish on mqtt and dweet.io.

--- README.md
+++ README.md
@@ -5,8 +5,46 @@ It also publish the data using Dweet.io to easily view it on e.g. freeboard.io.
 
 # To get started
 
-1. Copy config_default.conf to config.conf 
-2. Add your configuration to config.conf
+1. Copy config_default.ini to config.ini 
+2. Add your configuration to config.ini
 3. Install required libraries
 
 
+# Alarms
+
+The alarms below are being pushed if alarm is given. 
+A general alarm_ok = 1 if any of the alarms below are high.  
+
+"allarmi": [ // Alarms
+0, blade_blocked
+1, repositioning_error 
+2, outside_wire
+3, blade_blocked
+4, outside_wire
+5, mower_tilted
+6, error 
+7, error
+8, error
+9, collision_sensor_blocked
+10,  
+11, charge_error
+12, battery_error
+13, 
+14,
+15,
+16,
+17,
+18,
+19,
+20,
+21,
+22,
+23,
+24,
+25,
+26,
+27,
+28,
+29,
+30
+],
--- worx-landroid-mqtt.py
+++ worx-landroid-mqtt.py
@@ -1,60 +1,83 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
 # -*- coding: utf-8 -*-
 
-import ConfigParser
+try:
+   import ConfigParser as ConfigParser
+except: 
+   import configparser as ConfigParser
+
 import time
 import paho.mqtt.client as mqtt
 import dweepy
 import requests
 
-DEBUG = True
+DEBUG = False
+running = True
 
 Config = ConfigParser.ConfigParser()
-Config.read('config.conf')
+Config.read('config.ini')
 
 def on_connect(client, userdata, rc):
    if(DEBUG):
-      print "Connected with result code", str(rc)
-   # Subscribe on connect to reconnect if connection is lost
-   
+      print("Connected with result code" + str(rc))
+
 def on_message(client, userdata, msg):
    if(DEBUG):
-      print "Received new message "
-      print msg.topic, ": ", str(msg.payload)
-   pass
+      print("Received new message ")
+      print(msg.topic + ":  " + str(msg.payload))
+   #pass
 
 # Initiate mqtt-client
 mqttc = mqtt.Client()
 mqttc.on_connect = on_connect
 mqttc.on_message = on_message
-
-mqttc.connect(Config.get("Mqtt", "Host"), Config.get("Mqtt", "Port"), 60)
+mqttc.connect(Config.get("Mqtt", "Host"), int(Config.get("Mqtt", "Port")), 60)
 mqttc.loop_start()
 
-running = True
+def push_message(sub_topic, value):
+   # Push mqtt message
+   topic = Config.get("Mqtt", "BaseTopic") + sub_topic
+   mqttc.publish(topic, value)
+   # Dweet message
+   dweepy.dweet_for(Config.get("Dweet", "Thing"), {sub_topic: str(value)})
+
+def check_general(data):
+   push_message('battery', float(data['perc_batt']))
+
+def check_alarms(alarm_array):
+   alarm_ok = 1
+   alarms = ['blade_blocked',
+             'repositioning_error',
+             'outside_wire',
+             'blade_blocked',
+             'outside_wire',
+             'mower_tilted',
+             'error',
+             'error',
+             'error',
+             'collision_sensor_blocked',
+             '',
+             'charge_error',
+             'battery_error']
+
+   for i in range(len(alarms)):
+      if alarm_array[i]==1:
+         alarm_ok = 0
+         if(DEBUG):
+            print(alarms[i])
+         push_message('alarm/' + alarms[i] + '/', alarm_array[i])
+   
+   push_message('alarm/alarm_ok/', alarm_ok)
 
 while running:
    # Poll Landroid Worx
    response = requests.get(Config.get("Landroid", "Addr"), auth=(Config.get("Landroid", "User"),Config.get("Landroid", "Pin")))
    data = response.json()
-   
-   battery = "%.1f" % float(data['perc_batt'])
-   if(DEBUG):
-      print "Battery: ", battery
-   topic = Config.get("Mqtt", "BaseTopic") + "battery/"
-   #mqttc.publish("laxryggen/landroid/battery", battery);
-   mqttc.publish(topic, battery);
- 
-   # Dweet.io the average ourdoor temperature
-   try:
-      if(DEBUG):
-         print "Landroid-Worx data: ", battery
-      dweepy.dweet_for(Config.get("Dweet", "Thing"), {'battery': str(battery)})
-   except:
-      if(DEBUG):
-         print "Dweet error!"
-      pass
- 
+
+   # Check alarm status 
+   check_alarms(data['allarmi'])
+   check_general(data)
+
    # Wait, and then exit program  
    time.sleep(5)
    mqttc.loop_stop()
